c3781326ac210a6a6b799e1d8d65d0fae8359819
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-02-18 16:52:49 -0800

    finish endpoints for model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request
 from flask_cors import CORS
-from model import main_func
+from model import main_func, generate_quiz, generate_summary
 import json
 
 app = Flask(__name__)
@@ -15,3 +15,13 @@ def index():
         print("Processing video file.")
         transcript, summary, quiz = main_func('video.mp4')
         return json.dumps({"transcript": transcript, "summary": summary, "quiz": quiz}), 200
+    
+@app.route("/regen_quiz", methods=['POST'])
+def regen_quiz():
+    quiz = generate_quiz(request.form["transcript"])
+    return json.dumps({"quiz": quiz}), 200
+
+@app.route("/regen_summary", methods=['POST'])
+def regen_summary():
+    summary = generate_summary(request.form["transcript"])
+    return json.dumps({"summary": summary}), 200
